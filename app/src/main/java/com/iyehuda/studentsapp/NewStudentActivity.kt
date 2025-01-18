9495617f79b56b7490126ea132e1897e3e7fd247
Yehuda Chikvashvili <yehudaac1@gmail.com>
2025-01-18 13:42:43 +0200

    Add NewStudent activity
    * Added activity for adding new students
    * Renamed strings to snake case

---
+++ app/src/main/java/com/iyehuda/studentsapp/NewStudentActivity.kt
@@ -0,0 +1,11 @@
+package com.iyehuda.studentsapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class NewStudentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_student)
+    }
+}
