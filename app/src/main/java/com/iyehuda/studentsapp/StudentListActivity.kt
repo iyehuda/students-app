a946825ebfd365758358a220bcc79b675d0aadf9
Yehuda Chikvashvili <yehudaac1@gmail.com>
2025-01-18 10:33:31 +0200

    Init Android application
    Created an empty android application

---
+++ app/src/main/java/com/iyehuda/studentsapp/StudentListActivity.kt
@@ -0,0 +1,11 @@
+package com.iyehuda.studentsapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class StudentListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_student_list)
+    }
+}
